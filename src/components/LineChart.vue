214f4875647e9d7a0dc88c83d23268d483eac33c
lgesenn <gesenn9999@gmail.com>
2021-06-02 22:38:53 +0800

    Create LineChart.vue

---
+++ src/components/LineChart.vue
@@ -0,0 +1,75 @@
+<script>
+import { Bar, mixins } from 'vue-chartjs';
+const { reactiveProp } = mixins;
+export default {
+  extends: Bar,
+  mixins: [reactiveProp],
+  props: {
+    chartData: {
+      type: Array,
+      required: false,
+    },
+    chartLabels: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          xAxes: [
+            {
+              gridLines: {
+                display: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
+        responsive: true,
+        animation: {
+          // duration: 10,
+          easing: 'easeOutQuad',
+        },
+        maintainAspectRatio: false,
+        title: {
+          display: true,
+          text: 'Work Hour By Day',
+        },
+      },
+    };
+  },
+  mounted() {
+    this.renderChart(
+      {
+        labels: this.chartLabels,
+        datasets: [
+          {
+            label: 'Quantity',
+            borderColor: '#63C5DA',
+            pointBackgroundColor: 'black',
+            borderWidth: 1,
+            pointBorderColor: '#63C5DA',
+            backgroundColor: '#63C5DA',
+            data: this.chartData,
+          },
+        ],
+      },
+      this.options
+    );
+  },
+};
+</script>
